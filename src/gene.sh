4e5a4727300474336ac38184bae139b8529e5400
epeidli <peter.li@ericsson.com>
2018-06-27 13:44:15 +0800

    add gene.sh

---
+++ src/gene.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+files=$(ls ..)
+main=../index.html
+cat /dev/null > $main
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>李有道的博客</title></head><body><h1>TECH</h1><ul>' > $main
+for i in $files; do
+  if [ $i != 'index.html' ] && [ $i != 'getindex.sh' ] && [ $i != 'main.html' ]; then
+    echo '<li><a href="'$i'">'$i'</a></li>' >> $main
+  fi
+done
+echo '</ul></body></html>' >> $main
+echo '首页文件生成完毕'
